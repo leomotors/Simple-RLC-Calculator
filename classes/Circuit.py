4993ad6ee046420ba1724e1565dc8ba062c69f76
Leomotors (Windows) <59821765+Leomotors@users.noreply.github.com>
2021-07-11 21:56:48 +0700

    enough for today

--- classes/Circuit.py
+++ classes/Circuit.py
@@ -1,5 +1,6 @@
 import pygame as pg
-        
+
+
 class Circuit:
     def __init__(self):
         self.impedance = None
@@ -7,14 +8,29 @@ class Circuit:
         self.current = None
         self.v_phase = None
         self.i_phase = None
-        
+
+
 class SeriesCircuit(Circuit):
     def __init__(self):
         Circuit.__init__(self)
-        pass
+        self.components = []  # * Circuits or Components
+        self.impedance = None
+
+    def CalcImpedance(self):
+        self.impedance = sum(k.impedance for k in self.components)
+
+    def getImpedance(self) -> complex:
+        return self.impedance
 
 
 class ParallelCircuit(Circuit):
     def __init__(self):
         Circuit.__init__(self)
-        pass
+        self.components = []  # * Circuits or Components
+        self.impendance = None
+
+    def CalcImpedance(self):
+        self.impedance = 1 / sum(1/k.impedance for k in self.components)
+
+    def getImpedance(self) -> complex:
+        return self.impedance
--- classes/Component.py
+++ classes/Component.py
@@ -1,22 +1,29 @@
 import pygame as pg
 
+
 class Resistor:
     def __init__(self):
         self.resistance = None
         self.impedance = None
-        
+
     def CalcImpedance(self):
         self.impedance = self.resistance
+
 
 class Inductor:
     def __init__(self):
         self.inductance = None
         self.impedance = None
-        
+
     def CalcImpedance(self, ω):
         self.impedance = self.inductance * ω
+
 
 class Capacitor:
     def __init__(self):
         self.capacitance = None
         self.impedance = None
+
+    def CalcImpedance(self, ω):
+        self.impedance = 1 / (self.capacitance * ω)
+    
