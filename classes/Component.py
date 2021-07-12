33d296b785e85d0f75dffd89104926dff851827e
Leomotors (Windows) <59821765+Leomotors@users.noreply.github.com>
2021-07-12 09:50:18 +0700

    Component adding

--- classes/Component.py
+++ classes/Component.py
@@ -4,7 +4,7 @@ import pygame as pg
 class Resistor:
     def __init__(self):
         self.resistance = None
-        self.impedance = None
+        self.impedance: complex = None
 
     def CalcImpedance(self):
         self.impedance = self.resistance
@@ -13,17 +13,16 @@ class Resistor:
 class Inductor:
     def __init__(self):
         self.inductance = None
-        self.impedance = None
+        self.impedance: complex = None
 
     def CalcImpedance(self, ω):
-        self.impedance = self.inductance * ω
+        self.impedance = (self.inductance * ω)*1j
 
 
 class Capacitor:
     def __init__(self):
         self.capacitance = None
-        self.impedance = None
+        self.impedance: complex = None
 
     def CalcImpedance(self, ω):
         self.impedance = 1 / (self.capacitance * ω)
-    
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame as pg
 from classes.Sprite import *
 from classes.Toggle import Toggle
+from classes.Circuit import *
+from classes.Component import *
+
 import pyautogui
+
+import math
 
 pg.init()
 
@@ -32,6 +37,21 @@ for button in buttons:
 
 isParallel = Toggle(False, "Parallel Mode", (313, 560))
 
+MainCircuit = SeriesCircuit()
+
+Circuit_Voltage: float = float(pyautogui.prompt(
+    text="Enter Voltage (rms): ", title="Circuit Setup", default=""))
+
+Circuit_f_Input = pyautogui.prompt(
+    text="Enter Circuit Frequency or ω (Put 'Hz' if it is frequency)", title="Circuit Setup", default="")
+
+Circuit_ω = 0
+if "Hz" in Circuit_f_Input:
+    Circuit_ω = 2 * math.pi * Circuit_f_Input
+else:
+    Circuit_ω = Circuit_f_Input
+
+
 while True:
     screen.fill((255, 255, 255))
 
@@ -43,11 +63,34 @@ while True:
             pos = pg.mouse.get_pos()
             for button in buttons:
                 if button.checkCollide(pos):
-                    print("Collide with", button.text)
+                    if button.text == "Add R":
+                        temp = Resistor()
+                        temp.resistance = float(pyautogui.prompt(
+                            text="Enter Resistance: ", title="Add Resistor", default=""))
+                        if temp.resistance is None:
+                            pass
+                        temp.CalcImpedance()
+                        MainCircuit.components.append(temp)
+
+                    if button.text == "Add L":
+                        temp = Inductor()
+                        temp.inductance = float(pyautogui.prompt(
+                            text="Enter Inductance: ", title="Add Inductor", default=""))
+                        temp.CalcImpedance(Circuit_ω)
+                        MainCircuit.components.append(temp)
+
+                    if button.text == "Add C":
+                        temp = Capacitor()
+                        temp.capacitance = float(pyautogui.prompt(
+                            text="Enter Capacitance: ", title="Add Capacitor", default=""))
+                        MainCircuit.components.append(temp)
 
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 isParallel.toggleAndShow(screen, font)
+            if event.key == pg.K_RETURN:
+                MainCircuit.CalcImpedance()
+                print(MainCircuit.impedance)
 
     for button in buttons:
         button.show()
